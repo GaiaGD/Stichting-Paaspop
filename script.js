7e041a478c260ca7a0576c8c66373f9873f1c4d0
GaiaGD <gaia.digregorio@gmail.com>
2023-01-25 00:44:51 -0500

    dropdown

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(bar) {
+    bar.classList.toggle("change");
+  }
+
+function toggleDropdown(){
+  let dropdown = document.getElementById('dropdown')
+  dropdown.classList.toggle("d-none")
+}
